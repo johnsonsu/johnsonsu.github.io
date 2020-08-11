a4b4b2b69880ae027f15277725e6ca2c1f671118
Johnson Su <johnsonsu0603@hotmail.com>
2020-08-10 22:13:54 -0400

    Deploy Next.js to master

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bea5526cab1163bdb1fb";
+/******/ 	var hotCurrentHash = "7114f0c91825f8d6dc46";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
